25a69f3cf0786ce8fbca5ded3df80f6008dc05ee
profoundBoi <connor170103@gmail.com>
2025-10-29 00:42:42 +0200

    safe push

---
+++ Scripts/indexAnim.js
@@ -0,0 +1,30 @@
+gsap.from(".about", {
+    duration: 1.5,
+    y: -100,          // slide up from below
+    opacity: 0,      // start transparent
+    ease: "power3.out"
+});
+
+gsap.from(".about h1", {
+    duration: 1,
+    opacity: 0,
+    y: 50,
+    delay: 0.4,
+    ease: "power2.out"
+});
+
+gsap.from(".about p", {
+    duration: 1,
+    opacity: 0,
+    y: -30,
+    delay: 0.6,
+    ease: "power2.out"
+});
+
+gsap.from(".about #logo", {
+    duration: 1.2,
+    opacity: 0,
+    scale: 0.8,
+    delay: 0.8,
+    ease: "back.out(1.7)"
+});
